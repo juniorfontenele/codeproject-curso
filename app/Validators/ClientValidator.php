bdf03de1362299c5474e0d0fbed998a00f6d3af3
Junior Fontenele <lcbfjr@gmail.com>
2016-03-15 09:55:25 -0300

    Traducoes do Prettus Validator

--- app/Validators/ClientValidator.php
+++ app/Validators/ClientValidator.php
@@ -8,9 +8,7 @@
 namespace CodeProject\Validators;
 
 
-use Prettus\Validator\LaravelValidator;
-
-class ClientValidator extends LaravelValidator
+class ClientValidator extends CodeProjectValidator
 {
 
     protected $rules = [
@@ -18,12 +16,6 @@ class ClientValidator extends LaravelValidator
         'email' => 'required|email|max:255',
         'responsible' => 'required|max:255',
         'phone' => 'required'
-    ];
-
-    protected $messages = [
-        'required' => 'O campo :attribute é obrigatório',
-        'email' => 'O campo :attribute deve ser um e-mail válido',
-        'max' => 'O campo :attribute deve conter :max caracteres no máximo'
     ];
 
 }
---
+++ app/Validators/CodeProjectValidator.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Author: Junior Fontenele <lcbfjr@gmail.com>
+ * Date: 15/03/2016
+ * Time: 09:47
+ */
+
+namespace CodeProject\Validators;
+
+
+use Prettus\Validator\LaravelValidator;
+
+class CodeProjectValidator extends LaravelValidator
+{
+    protected $messages = [
+        'required' => 'O campo :attribute é obrigatório',
+        'email' => 'O campo :attribute deve ser um e-mail válido',
+        'max' => 'O campo :attribute deve conter :max caracteres no máximo',
+        'min' => 'O campo :attribute deve conter :max caracteres no mínimo',
+        'between' => 'O campo :attribute deve estar entre :min e :max',
+        'integer' => 'O campo :attribute deve conter um número inteiro'
+    ];
+}
